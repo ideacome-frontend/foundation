e8695bf822081016102a1e60baaf2d1cf85130d0
Losan <shenglong.lu@ideacome.com>
2018-04-04 17:10:06 +0800

    [refactor](rules)rules验证规则统一使用ideacome foundation

--- validators/user-info.d.ts
+++ validators/user-info.d.ts
@@ -2,7 +2,7 @@
  * 验证身份证号码
  * @param idNumber 身份证号码
  */
-export declare function isValidID(idNumber: string): boolean;
+export declare function isValidID(idNumber?: string): boolean;
 /**
  * 验证邮箱
  * @param email 邮箱
@@ -18,4 +18,9 @@ export declare function isValidCellphone(phone: string): boolean;
  * @param cardNumber 银行卡号
  */
 export declare function isValidBankCardNumber(cardNumber: string): boolean;
+/**
+ * 验证密码
+ * 规则：8-16位数字和字母组成的字符串
+ * */
+export declare function isValidPassword(password: any): boolean;
 export { isAllChinese, hasChinese } from './string';
