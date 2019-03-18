2912f2d71d52980832d4c2484fbef7b2ab277e8f
liaokun <kun.liao@ideacome.com>
2019-03-18 10:25:10 +0800

    [refactor]()npm run build

--- validators/index.js
+++ validators/index.js
@@ -2,6 +2,3 @@ export * from './car-info';
 export * from './user-info';
 export * from './string';
 export * from './date';
-export * from './dom';
-export * from './formatter';
-export * from './image';
